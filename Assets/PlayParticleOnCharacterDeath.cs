3b772c590513518b0f5af48873601f75100b1509
DavidNishball <DavidENishball@gmail.com>
2017-03-13 19:53:51 -0700

    Implemented a particle on character death

---
+++ Assets/PlayParticleOnCharacterDeath.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayParticleOnCharacterDeath : MonoBehaviour {
+
+    public Robo_CharacterController targetCharacter;
+    ParticleSystem particle;
+
+    // This allows us to re-use the same particle over and over.
+	void Start()
+    {
+        particle = GetComponent<ParticleSystem>();
+
+        targetCharacter.Died += Character_Died;
+    }
+
+    private void Character_Died(Robo_CharacterController character)
+    {
+        particle.transform.position = character.transform.position;
+        particle.Clear();
+        particle.Play();
+    }
+}
